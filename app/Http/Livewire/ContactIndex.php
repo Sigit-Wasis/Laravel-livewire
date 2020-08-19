d004eee7a4f022021334c0bbb36b06e83cd7235a
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-08-19 11:10:00 +0700

    create and validation

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -7,10 +7,19 @@ use Livewire\Component;
 
 class ContactIndex extends Component
 {
+	protected $listeners = [
+		'contactStored' => 'handleStored'
+	];
+
     public function render()
     {
         return view('livewire.contact-index', [
         	'contacts' => Contact::latest()->get()
         ]);
+    }
+
+    public function handleStored($contact)
+    {
+    	session()->flash('message', 'Contact ' . $contact['name'] . ' was stored!');
     }
 }
--- resources/views/livewire/contact-create.blade.php
+++ resources/views/livewire/contact-create.blade.php
@@ -3,10 +3,20 @@
 		<div class="form-group">
 			<div class="form-row">
 				<div class="col">
-					<input wire:model="name" type="text" name="name" class="form-control" placeholder="Name">
+					<input wire:model="name" type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Name">
+					@error('name')
+						<span class="invalid-feedback">
+							<strong>{{ $message }}</strong>
+						</span>
+					@enderror
 				</div>
 				<div class="col">
-					<input wire:model="phone" type="text" name="phone" class="form-control" placeholder="Phone">
+					<input wire:model="phone" type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" placeholder="Phone">
+					@error('phone')
+						<span class="invalid-feedback">
+							<strong>{{ $message }}</strong>
+						</span>
+					@enderror
 				</div>
 			</div>
 		</div>
